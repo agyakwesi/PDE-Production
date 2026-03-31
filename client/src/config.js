1804f175d712af6246512be1c2e3b5bf02f2ace5
Agya Kwesi Adom Manu-Ofosu <akamanuofosu@st.knust.edu.gh>
2026-03-31 02:08:30 +0000

    config: update Heroku URL suffix to correct value

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || (import.meta.env.MODE === 'production' ? 'https://parfum-d-elie.herokuapp.com' : 'http://localhost:5000');
+const API_BASE_URL = import.meta.env.VITE_API_URL || (import.meta.env.MODE === 'production' ? 'https://parfum-d-elie-1cc70f4bba0d.herokuapp.com' : 'http://localhost:5000');
 
 export default API_BASE_URL;
